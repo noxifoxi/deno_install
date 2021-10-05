0de61c55292e4f0cbc20352d72c9e8bd9687129b
Foxi <293639+noxifoxi@users.noreply.github.com>
2021-10-05 17:49:11 +0200

    fixed test (because the minimum available version is 1.6.3)

--- install_test.sh
+++ install_test.sh
@@ -9,12 +9,12 @@ sh ./install.sh
 ~/.deno/bin/deno --version
 
 # Test that we can install a specific version at a custom location.
-rm -rf ~/deno-1.0.0
-export DENO_INSTALL="$HOME/deno-1.0.0"
-./install.sh v1.0.0
-~/deno-1.0.0/bin/deno --version | grep 1.0.0
+rm -rf ~/deno-1.6.3
+export DENO_INSTALL="$HOME/deno-1.6.3"
+./install.sh v1.6.3
+~/deno-1.6.3/bin/deno --version | grep 1.6.3
 
 # Test that we can install at a relative custom location.
 export DENO_INSTALL="."
-./install.sh v1.1.0
-bin/deno --version | grep 1.1.0
+./install.sh v1.8.0
+bin/deno --version | grep 1.8.0
